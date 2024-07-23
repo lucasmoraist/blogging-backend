db83f9a249dae46aec2e96f0200401974a641c74
lucasmoraist <luksmnt1101@gmail.com>
2024-07-23 11:36:18 -0300

    feat: Add User entity to appDataSource

--- src/lib/typeorm/typeorm.ts
+++ src/lib/typeorm/typeorm.ts
@@ -1,5 +1,6 @@
 import { Post } from '@/entities/post.entity';
 import { Teacher } from '@/entities/teacher.entity';
+import { User } from '@/entities/user.entity';
 import { env } from '@/env';
 import { DataSource } from 'typeorm';
 
@@ -10,7 +11,7 @@ export const appDataSource = new DataSource({
   username: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
-  entities: [Post, Teacher],
+  entities: [Post, Teacher, User],
   logging: env.NODE_ENV === 'development',
 });
 
