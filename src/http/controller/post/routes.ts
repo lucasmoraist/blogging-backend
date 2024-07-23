3e731ab4e53ab6ded76bbceb03eb9a707373f073
lucasmoraist <luksmnt1101@gmail.com>
2024-07-23 11:36:37 -0300

    chore: Update routes to include JWT validation middleware

--- src/http/controller/post/routes.ts
+++ src/http/controller/post/routes.ts
@@ -4,11 +4,12 @@ import { findOne } from './find-one';
 import { update } from './update';
 import { deletePost } from './delete';
 import { findAll } from './find-all';
+import { jwtValidate } from '@/http/middleware/jwt-validate';
 
 export async function postRoutes(app: FastifyInstance) {
-  app.post('/post/:teacherId', create);
+  app.post('/post/:teacherId', { onRequest: jwtValidate }, create);
   app.get('/post', findAll);
   app.get('/post/:id', findOne);
-  app.put('/post/:id', update);
-  app.delete('/post/:id', deletePost);
+  app.put('/post/:id', { onRequest: jwtValidate }, update);
+  app.delete('/post/:id', { onRequest: jwtValidate }, deletePost);
 }
--- src/http/controller/teacher/routes.ts
+++ src/http/controller/teacher/routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { create } from './create';
 import { findTeacher } from './find-one';
+import { jwtValidate } from '@/http/middleware/jwt-validate';
 
 export async function teacherRoutes(app: FastifyInstance) {
-  app.post('/teacher', create);
-  app.get('/teacher/:id', findTeacher);
+  app.post('/teacher', { onRequest: jwtValidate }, create);
+  app.get('/teacher/:id', { onRequest: jwtValidate }, findTeacher);
 }
