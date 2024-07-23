f6f13101024334d658c03e498f8f99b2086d3b03
lucasmoraist <luksmnt1101@gmail.com>
2024-07-23 11:34:53 -0300

    chore: Update app.ts to register userRoutes and configure JWT authentication

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,17 @@ import 'reflect-metadata';
 import fastify from 'fastify';
 import { teacherRoutes } from './http/controller/teacher/routes';
 import { postRoutes } from './http/controller/post/routes';
+import fastifyJwt from '@fastify/jwt';
+import { env } from './env/index';
+import { userRoutes } from './http/controller/user/routes';
 
-export const app = fastify();
+export const app = fastify({ logger: true });
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: { expiresIn: '100m' },
+});
+
+app.register(userRoutes);
 app.register(teacherRoutes);
 app.register(postRoutes);
