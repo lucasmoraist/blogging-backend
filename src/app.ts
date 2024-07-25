8386f6f983edb241e9cc1d306403cff63951955a
lucasmoraist <luksmnt1101@gmail.com>
2024-07-25 10:50:33 -0300

    chore: Register Swagger UI and configure routes

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,21 @@ import { postRoutes } from './http/controller/post/routes';
 import fastifyJwt from '@fastify/jwt';
 import { env } from './env/index';
 import { userRoutes } from './http/controller/user/routes';
+import swagger from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
+import { swaggerConfig } from './lib/swagger/swagger-config';
 
 export const app = fastify({ logger: true });
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   sign: { expiresIn: '100m' },
+});
+
+app.register(swagger, swaggerConfig);
+
+app.register(swaggerUI, {
+  routePrefix: '/',
 });
 
 app.register(userRoutes);
