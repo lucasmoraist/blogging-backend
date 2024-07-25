3c451a7d6d2e0da652511e3f477913da820b9c16
lucasmoraist <luksmnt1101@gmail.com>
2024-07-25 11:46:49 -0300

    chore: Update npm dependencies and add CORS support

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,15 @@ import { userRoutes } from './http/controller/user/routes';
 import swagger from '@fastify/swagger';
 import swaggerUI from '@fastify/swagger-ui';
 import { swaggerConfig } from './lib/swagger/swagger-config';
+import cors from '@fastify/cors';
 
 export const app = fastify({ logger: true });
+
+app.register(cors, {
+  origin: '*',
+  allowedHeaders: '*',
+  methods: '*',
+});
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
